f9fcdd765e327813df05802720c9ba48128f0882
Dragan1716 <dragan.badnjar17@gmail.com>
2024-07-09 11:53:12 +0200

    Home component

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Row title="Trending Now" fetchUrl={requests.requestTrending} />
+      <Row title="Top Rated" fetchUrl={requests.requestPopular} />
+      <Row title="Horror" fetchUrl={requests.requestHorror} />
+      <Row title="Comedy" fetchUrl={requests.requestTrending} />
+      <Row title="Up Coming" fetchUrl={requests.requestUpcoming} />
+    </>
+  );
+};
+
+export default Home;

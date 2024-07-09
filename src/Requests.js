bab5ad995e4294ac829fd2e323e0f32cc40cc166
Dragan1716 <dragan.badnjar17@gmail.com>
2024-07-09 11:46:08 +0200

    Requests component

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const key = "607b26a270d99be23078ee30bcdd5bcf";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=3`,
+  requestHorror: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=2`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+// https://api.themoviedb.org/3/movie/550?api_key=607b26a270d99be23078ee30bcdd5bcf
+
+export default requests;

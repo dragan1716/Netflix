78644abdf299b3a55506a68349fb5111c01d0808
Dragan1716 <dragan.badnjar17@gmail.com>
2024-07-10 22:34:40 +0200

    Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -29,7 +29,7 @@ const Movie = ({ movie }) => {
   return (
     <div
       key={movie.id}
-      className="w-160px sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+      className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
     >
       <img
         className="w-full h-auto block object-cover"
@@ -37,11 +37,11 @@ const Movie = ({ movie }) => {
         alt={movie?.title}
       />
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 text-white opacity-0 hover:opacity-100">
-        <p className="flex justify-center items-center font-bold px-2 h-full w-full top-[40%] text-center">
+        <p className="whitespace-normal flex justify-center items-center font-bold px-2 h-full w-full top-[40%] text-center break-words">
           {movie?.title}
         </p>
         <div onClick={handleLikeClick} className="absolute top-4 left-4">
-          {liked ? <FaHeart /> : <FaRegHeart />}
+          {liked ? <FaHeart className="text-red-500" /> : <FaRegHeart />}
         </div>
       </div>
     </div>

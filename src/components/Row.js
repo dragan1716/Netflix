5574c96a13c889945d98019c919b634e0147eaee
Dragan1716 <dragan.badnjar17@gmail.com>
2024-07-10 10:21:55 +0200

    Replacing code with Movie component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import Movie from "./Movie";
 
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
@@ -25,21 +27,7 @@ const Row = ({ title, fetchUrl }) => {
       <div className="relative flex items-center">
         <div id={"slider"} className="flex">
           {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="w-160px sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
-            >
-              <img
-                className="w-full h-auto block object-cover"
-                src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-                alt={movie?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 text-white opacity-0 hover:opacity-100">
-                <p className="flex justify-center items-center font-bold px-2 h-full w-full top-[40%] text-center">
-                  {movie?.title}
-                </p>
-              </div>
-            </div>
+            <Movie key={movie.id} movie={movie} />
           ))}
         </div>
       </div>
